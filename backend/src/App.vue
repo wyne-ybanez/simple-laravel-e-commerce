a32f067e34652563c8917e9284d55f3625697e2b
wyne-ybanez <wyne_01@icloud.com>
2023-01-25 14:59:06 +0000

    install tailwind

--- backend/src/App.vue
+++ backend/src/App.vue
@@ -3,6 +3,10 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
@@ -11,19 +15,6 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
+  
   <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
