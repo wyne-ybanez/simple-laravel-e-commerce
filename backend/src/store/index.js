36b470e6a87d12be98ca821759810266f771ca45
wyne-ybanez <wyne_01@icloud.com>
2023-01-25 16:11:21 +0000

    installs vuex and creates store

---
+++ backend/src/store/index.js
@@ -0,0 +1,12 @@
+import { createStore } from "vuex";
+
+const store = createStore({
+    state: {
+        test: '567'
+    },
+    getters: {},
+    actions: {},
+    mutations: {},
+})
+
+export default store;
