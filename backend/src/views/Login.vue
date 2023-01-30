143a99d0157331122d246b789ad829630c8253a8
wyne-ybanez <wyne_01@icloud.com>
2023-01-30 11:32:11 +0000

    adds reset password layouts

--- backend/src/views/Login.vue
+++ backend/src/views/Login.vue
@@ -1,22 +1,8 @@
 <template>
-  <!--
-    This example requires updating your template:
-
-    ```
-    <html class="h-full bg-gray-50">
-    <body class="h-full">
-    ```
-  -->
   <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
     <div class="w-full max-w-md space-y-8">
       <div>
-        <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" />
         <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Sign in to your account</h2>
-        <p class="mt-2 text-center text-sm text-gray-600">
-          Or
-          {{ ' ' }}
-          <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">start your 14-day free trial</a>
-        </p>
       </div>
       <form class="mt-8 space-y-6" action="#" method="POST">
         <input type="hidden" name="remember" value="true" />
@@ -38,7 +24,7 @@
           </div>
 
           <div class="text-sm">
-            <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">Forgot your password?</a>
+            <router-link :to="{name:'requestPassword'}" class="font-medium text-indigo-600 hover:text-indigo-500">Forgot your password?</router-link>
           </div>
         </div>
 
@@ -55,10 +41,10 @@
   </div>
 </template>
 
-<script setup>
+<script>
 import { LockClosedIcon } from '@heroicons/vue/20/solid'
 
-// export default {
-//     name: "Login"
-// }
+export default {
+    name: "Login"
+}
 </script>
