abe79216324e760c7fb306fde96c49118dd0e151
wyne-ybanez <wyne_01@icloud.com>
2023-02-03 12:15:56 +0000

    abstracts guest layout

--- backend/src/views/Login.vue
+++ backend/src/views/Login.vue
@@ -1,10 +1,6 @@
 <template>
-  <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-    <div class="w-full max-w-md space-y-8">
-      <div>
-        <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Sign in to your account</h2>
-      </div>
-      <form class="mt-8 space-y-6" action="#" method="POST">
+  <GuestLayout title="Sign in to your account">
+    <form class="mt-8 space-y-6" method="POST" @submit.prevent="login">
         <input type="hidden" name="remember" value="true" />
         <div class="-space-y-px rounded-md shadow-sm">
           <div>
@@ -36,15 +32,12 @@
             Sign in
           </button>
         </div>
-      </form>
-    </div>
-  </div>
+    </form>
+  </GuestLayout>
 </template>
 
-<script>
+<script setup>
 import { LockClosedIcon } from '@heroicons/vue/20/solid'
+import GuestLayout from '../components/GuestLayout.vue'
 
-export default {
-    name: "Login"
-}
 </script>
