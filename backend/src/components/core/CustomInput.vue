67a9631e2ab7b426128f6673fde933eaf1c73848
wyne-ybanez <wyne_01@icloud.com>
2023-03-18 23:54:30 +0000

    refactor custom inputs in product modal

--- backend/src/components/core/CustomInput.vue
+++ backend/src/components/core/CustomInput.vue
@@ -1,75 +1,94 @@
 <template>
-     <div>
+  <div>
     <label class="sr-only">{{ label }}</label>
     <div class="mt-8 flex rounded-sm shadow-sm">
-      <span v-if="prepend"
-            class="inline-flex items-center mt-8 px-3 h-12 rounded-l-sm border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm">
+      <!-- <span v-if="prepend"
+            class="inline-flex items-center px-3 rounded-l-sm border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm">
         {{ prepend }}
-      </span>
+      </span> -->
       <template v-if="type === 'select'">
-         <div class="absolute">
-            {{ label }}
-            <span v-if="required" class="text-zinc-400 text-lg">*</span>
-        </div>
-        <select :name="name"
-                :required="required"
-                :value="props.modelValue"
-                :class="inputClasses"
-                @change="onChange($event.target.value)">
-          <option v-for="option of selectOptions" :value="option.key">{{option.text}}</option>
-        </select>
-      </template>
-      <template v-else-if="type === 'textarea'">
-        <div class="absolute">
-            {{ label }}
-            <span v-if="required" class="text-zinc-400 text-lg">*</span>
-        </div>
-        <textarea :name="name"
+       <div class="flex flex-col grow">
+            <div>
+                {{ label }}
+                <span v-if="required" class="text-zinc-400 text-lg">*</span>
+            </div>
+            <select :name="name"
                     :required="required"
                     :value="props.modelValue"
-                    @input="emit('update:modelValue', $event.target.value)"
-                    :class="inputClasses">
-        </textarea>
+                    :class="inputClasses"
+                    @change="onChange($event.target.value)">
+            <option v-for="option of selectOptions" :value="option.key">{{option.text}}</option>
+            </select>
+        </div>
+      </template>
+      <template v-else-if="type === 'textarea'">
+        <div class="flex flex-col grow">
+            <div>
+                {{ label }}
+                <span v-if="required" class="text-zinc-400 text-lg">*</span>
+            </div>
+            <textarea :name="name"
+                        :required="required"
+                        :value="props.modelValue"
+                        @input="emit('update:modelValue', $event.target.value)"
+                        :class="inputClasses">
+            </textarea>
+        </div>
       </template>
       <template v-else-if="type === 'file'">
-        <div class="absolute">
-            {{ label }}
-            <span v-if="required" class="text-zinc-400 text-lg">*</span>
+        <div class="flex flex-col grow">
+            <div>
+                {{ label }}
+                <span v-if="required" class="text-zinc-400 text-lg">*</span>
+            </div>
+            <input :type="type"
+                :name="name"
+                :required="required"
+                :value="props.modelValue"
+                @input="emit('change', $event.target.files[0])"
+                :class="inputClasses"
+                :placeholder="label"/>
         </div>
-        <input :type="type"
-               :name="name"
-               :required="required"
-               :value="props.modelValue"
-               @input="emit('change', $event.target.files[0])"
-               :class="inputClasses"
-               :placeholder="label"/>
       </template>
       <template v-else-if="type === 'checkbox'">
-        <div class="absolute">
-            {{ label }}
-            <span v-if="required" class="text-zinc-400 text-lg">*</span>
+        <div class="flex flex-col grow">
+            <div>
+                {{ label }}
+                <span v-if="required" class="text-zinc-400 text-lg">*</span>
+            </div>
+            <input :id="id"
+                :name="name"
+                :type="type"
+                :checked="props.modelValue"
+                :required="required"
+                @change="emit('update:modelValue', $event.target.checked)"
+                class="h-4 w-4 text-zinc-600 border-gray-300 rounded"/>
+            <label :for="id" class="ml-2 block text-sm text-gray-900"> {{ label }} </label>
         </div>
-        <input :id="id"
-               :name="name"
-               :type="type"
-               :checked="props.modelValue"
-               :required="required"
-               @change="emit('update:modelValue', $event.target.checked)"
-               class="h-4 w-4 text-zinc-600 border-gray-300 rounded"/>
-        <label :for="id" class="ml-2 block text-sm text-gray-900"> {{ label }} </label>
       </template>
       <template v-else>
-        <div class="absolute">
-            {{ label }}
-            <span class="text-zinc-400 text-lg">*</span>
+        <div class="flex flex-col grow">
+            <div>
+                {{ label }}
+                <span class="text-zinc-400 text-lg">*</span>
+            </div>
+            <input v-if="label === 'Price' "
+                placeholder="€"
+                :type="type"
+                :name="name"
+                :required="required"
+                :value="props.modelValue"
+                @input="emit('update:modelValue', $event.target.value)"
+                :class="inputClasses"
+                step="0.01"/>
+            <input v-else
+                :type="type"
+                :name="name"
+                :required="required"
+                :value="props.modelValue"
+                @input="emit('update:modelValue', $event.target.value)"
+                :class="inputClasses"/>
         </div>
-        <input :type="type"
-               :name="name"
-               :required="required"
-               :value="props.modelValue"
-               @input="emit('update:modelValue', $event.target.value)"
-               :class="inputClasses"
-               step="0.01"/>
       </template>
       <span v-if="append"
             class="inline-flex items-center px-3 rounded-r-sm border border-l-0 border-gray-300 bg-gray-50 text-gray-500 text-sm">
@@ -105,7 +124,7 @@ const props = defineProps({
 
 const inputClasses = computed(() => {
     const cls = [
-        `block w-full mt-8 px-3 py-3 border border-gray-300 bg-zinc-50 placeholder-gray-500 text-zinc-900 focus:border-gray-400 focus:bg-white focus:outline-none focus:ring-0 focus:ring-offset-0 sm:text-sm`,
+        `w-full mt-3 px-3 py-3 border border-gray-300 bg-zinc-50 placeholder-gray-500 text-zinc-900 focus:border-gray-400 focus:bg-white focus:outline-none focus:ring-0 focus:ring-offset-0 sm:text-sm`,
     ];
 
     if (props.append && !props.prepend) {
--- backend/src/store/actions.js
+++ backend/src/store/actions.js
@@ -1,46 +1,77 @@
 import axiosClient from "../axios";
 
-export function getUser({commit}, data) {
-  return axiosClient.get('/user', data)
-    .then(({data}) => {
-      commit('setUser', data);
-      return data;
-    })
+export function getUser({ commit }, data) {
+    return axiosClient.get("/user", data).then(({ data }) => {
+        commit("setUser", data);
+        return data;
+    });
 }
 
-export function login({commit}, data) {
-  return axiosClient.post('/login', data)
-    .then(({data}) => {
-      commit('setUser', data.user);
-      commit('setToken', data.token)
-      return data;
-    })
+export function login({ commit }, data) {
+    return axiosClient.post("/login", data).then(({ data }) => {
+        commit("setUser", data.user);
+        commit("setToken", data.token);
+        return data;
+    });
 }
 
-export function logout({commit}) {
-  return axiosClient.post('/logout')
-    .then((response) => {
-      commit('setToken', null)
+export function logout({ commit }) {
+    return axiosClient.post("/logout").then((response) => {
+        commit("setToken", null);
 
-      return response;
-    })
+        return response;
+    });
 }
 
-export function getProducts({commit}, {url = null, search = '', perPage = 10, sort_field, sort_direction, }) {
-  commit('setProduct', [true])
-  url = url || '/product'
-  return axiosClient.get(url, {
-    params: {
-      search,
-      per_page: perPage,
-      sort_field,
-      sort_direction,
+export function getProducts(
+    { commit },
+    { url = null, search = "", perPage = 10, sort_field, sort_direction }
+) {
+    commit("setProduct", [true]);
+    url = url || "/product";
+    return axiosClient
+        .get(url, {
+            params: {
+                search,
+                per_page: perPage,
+                sort_field,
+                sort_direction,
+            },
+        })
+        .then((res) => {
+            commit("setProduct", [false, res.data]);
+        })
+        .catch(() => {
+            commit("setProduct", [false]);
+        });
+}
+
+export function createProduct({ commit }, product) {
+    if (product.image instanceof File) {
+        const form = new FormData();
+        form.append("title", product.title);
+        form.append("image", product.image);
+        form.append("description", product.description);
+        form.append("price", product.price);
+        product = form;
     }
-  })
-    .then(res => {
-      commit('setProduct', [false, res.data])
-    })
-    .catch(() => {
-      commit("setProduct", [false]);
-    })
+    return axiosClient.post("/product", product);
+}
+
+export function updateProduct({ commit }, product) {
+  const id = product.id
+  if (product.image instanceof File) {
+      const form = new FormData();
+      form.append("id", product.id);
+      form.append("title", product.title);
+      form.append("image", product.image);
+      form.append("description", product.description);
+      form.append("price", product.price);
+      form.append("_method", "PUT");
+      product = form;
+  } else {
+      // laravel understands this as an Update
+      product._method = "PUT";
+  }
+  return axiosClient.post(`/product/${id}`, product);
 }
