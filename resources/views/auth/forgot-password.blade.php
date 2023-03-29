dbfdeb0944f999a9da6dc416bc76d78203026c7d
wyne-ybanez <wyne_01@icloud.com>
2023-03-29 15:41:06 +0100

    update: changes password reset to store theme

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,12 +1,18 @@
 <x-app-layout>
-    <div class="mb-4 mt-20 text-lg text-gray-900 dark:text-stone-900 text-center">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
-    </div>
+    <h2 class="text-2xl font-semibold text-center mb-2 mt-32">
+        Reset your password
+    </h2>
+    <p class="text-center text-gray-500">
+        or
+        <a href="{{ route('login') }}" class="text-sm text-primary hover:text-black">
+            login to your account
+        </a>
+    </p>
 
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form method="POST" action="{{ route('password.email') }}" class="w-[450px] mx-auto p-6 my-28">
+    <form method="POST" action="{{ route('password.email') }}" class="w-[450px] mx-auto p-6">
         @csrf
 
         <!-- Email Address -->
@@ -16,10 +22,16 @@
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-end mt-8">
+        <div class="flex items-center justify-end mt-6">
             <x-primary-button>
                 {{ __('Reset Password') }}
             </x-primary-button>
         </div>
     </form>
+
+    <div class="mb-4 mt-20 text-base text-gray-900 dark:text-stone-900 w-[450px] mx-auto">
+        <p>Forgot your password? No problem.</p>
+        <br>
+        <p>Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.</p>
+    </div>
 </x-app-layout>
