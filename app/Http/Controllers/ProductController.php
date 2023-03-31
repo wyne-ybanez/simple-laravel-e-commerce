290b957eba434f6a8cad01cbad88ba49afe7c388
wyne-ybanez <wyne_01@icloud.com>
2023-03-31 15:24:48 +0100

    feat: adds customized pagination

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class ProductController extends Controller
 {
     public function index() {
-        $products = Product::query()->orderBy('updated_at', 'desc')->paginate(12);
+        $products = Product::query()->orderBy('updated_at', 'asc')->paginate(6);
 
         return view('product.index', [
             'products' => $products
--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -25,7 +25,7 @@
                     <h5 class="font-bold text-2xl font-montserrat">€{{$product->price}}</h5>
                 </div>
                 <div class="flex">
-                    <button @click="addToWatchlist()" class="w-10 h-10 mr-2 rounded-sm border border-1 border-stone-600 flex items-center justify-center hover:bg-stone-600 hover:text-white active:bg-stone-800 transition-colors" :class="isInWatchlist(id) ? 'bg-stone-600 text-white' : 'text-stone-600'">
+                    <button @click="addToWatchlist()" class="w-10 h-10 mr-2 rounded-sm border border-2 border-bg-strong flex items-center justify-center hover:bg-stone-600 hover:border-stone-600 hover:text-white active:bg-stone-800 transition-colors" :class="isInWatchlist(id) ? 'bg-stone-600 text-white' : 'text-strong'">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
                         </svg>
@@ -38,5 +38,9 @@
 
         </div>
         @endforeach
+    </div>
+
+    <div class="p-5 product-pagination">
+        {{ $products->links() }}
     </div>
 </x-app-layout>
