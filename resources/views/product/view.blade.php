4495e1d4f0cfbce585315b539b7ef577225f0e05
wyne-ybanez <wyne_01@icloud.com>
2023-04-18 17:19:24 +0100

    updates: minor style updates to product template

--- resources/views/product/view.blade.php
+++ resources/views/product/view.blade.php
@@ -55,22 +55,9 @@
             </div>
 
             <div class="lg:col-span-1 w-fit px-[2rem] py-10">
-                <h1 class="text-lg font-semibold">
+                <h1 class="lg:text-7xl text-4xl font-semibold mb-2">
                     {{$product->title}}
                 </h1>
-                <div class="text-xl font-bold mb-6">${{$product->price}}</div>
-                <div class="flex items-center justify-between mb-5">
-                    <label for="quantity" class="block font-bold mr-4">
-                        Quantity
-                    </label>
-                    <input type="number" name="quantity" x-ref="quantityEl" value="1" min="1" class="w-32 focus:border-purple-500 focus:outline-none rounded" />
-                </div>
-                <button @click="addToCart($refs.quantityEl.value)" class="btn-primary py-4 text-lg flex justify-center min-w-0 w-full mb-6">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-                    </svg>
-                    Add to Cart
-                </button>
                 <div class="mb-6" x-data="{expanded: false}">
                     <div x-show="expanded" x-collapse.min.120px class="text-black wysiwyg-content">
                         {{ $product->description }}
@@ -78,6 +65,22 @@
                     <p class="text-right">
                         <a @click="expanded = !expanded" href="javascript:void(0)" class="text-stone-500 hover:text-stone-700" x-text="expanded ? 'Read Less' : 'Read More'"></a>
                     </p>
+                </div>
+
+                <hr class="h-[2px] bg-black mb-10">
+
+                <div class="text-xl font-bold mb-6">${{$product->price}}</div>
+                <div class="flex items-center mb-5">
+                    <label for="quantity" class="block font-bold mr-4">
+                        Quantity
+                    </label>
+                    <input type="number" name="quantity" x-ref="quantityEl" value="1" min="1" class="w-32 focus:border-stone-500 focus:outline-none rounded" />
+                    <button @click="addToCart($refs.quantityEl.value)" class="btn-primary py-4 text-lg flex ml-auto min-w-0 w-fit mb-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                        </svg>
+                        Add to Cart
+                    </button>
                 </div>
             </div>
         </div>
