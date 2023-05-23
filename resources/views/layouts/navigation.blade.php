011f4e8211fd90be70a5f5c47c8397f37e6c2298
wyne-ybanez <wyne_01@icloud.com>
2023-05-23 12:59:24 +0100

    context: prepare routes for cart API

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -11,7 +11,7 @@
                 </div>
             </li>
             <li>
-                <a href="/src/index.html" class="relative flex items-center justify-between py-3 px-6 transition-colors hover:bg-gray-100">
+                <a href="/" class="relative flex items-center justify-between py-3 px-6 transition-colors hover:bg-gray-100">
                     <div class="flex items-center">
                         <div class="link-underline link-underline-black text-stone-600">
                             Home
@@ -137,7 +137,7 @@
                 </div>
             </li>
             <li>
-                <a href="/src/cart.html" class="relative inline-flex items-center py-navbar-item px-navbar-item hover:text-primary">
+                <a href="{{ route('cart') }}" class="relative inline-flex items-center py-navbar-item px-navbar-item hover:text-primary">
                     <div class="link-underline link-underline-black">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
--- routes/web.php
+++ routes/web.php
@@ -16,20 +16,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [ProductController::class, 'index'])->name('home');
+Route::middleware(['guestOrVerified'])->group(function() {
+    Route::get('/', [ProductController::class, 'index'])->name('home');
+    Route::get('/monsters', [ProductController::class, 'category1'])->name('monsters');
+    Route::get('/anti-heroes', [ProductController::class, 'category2'])->name('anti_heroes');
+    Route::get('/heroes', [ProductController::class, 'category3'])->name('heroes');
+    Route::get('/landscapes', [ProductController::class, 'category4'])->name('landscapes');
+    Route::get('/product/{product:slug}', [ProductController::class, 'view'])->name('product.view');
 
-Route::get('/monsters', [ProductController::class, 'category1'])->name('monsters');
-Route::get('/anti-heroes', [ProductController::class, 'category2'])->name('anti_heroes');
-Route::get('/heroes', [ProductController::class, 'category3'])->name('heroes');
-Route::get('/landscapes', [ProductController::class, 'category4'])->name('landscapes');
+    Route::prefix('/cart')->name('cart.')->group(Function() {
+        Route::get('/', [CartController::class, 'index'])->name('index');
+        Route::get('/add/{product:slug}', [CartController::class, 'add'])->name('add');
+        Route::get('/remove/{product:slug}', [CartController::class, 'remove'])->name('remove');
+        Route::get('/update-quantity/{product:slug}', [CartController::class, 'updateQuantity'])->name('update-quantity');
+    });
+});
 
 Route::get('/cart', [CartController::class, 'index'])->name('cart');
 
-Route::get('/product/{product:slug}', [ProductController::class, 'view'])->name('product.view');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
