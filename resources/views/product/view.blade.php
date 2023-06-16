228a3dbdad3932377117279cd741fd2fcc8b85e7
wyne-ybanez <wyne_01@icloud.com>
2023-06-16 15:37:17 +0100

    fix: adds fillable props, refers to quantity val when adding to cart

--- resources/views/product/view.blade.php
+++ resources/views/product/view.blade.php
@@ -66,7 +66,7 @@
                         Quantity
                     </label>
                     <input type="number" name="quantity" x-ref="quantityEl" value="1" min="1" class="w-32 focus:border-stone-500 focus:outline-none rounded" />
-                    <button @click="addToCart()" class="btn-primary py-4 text-lg flex ml-auto min-w-0 w-fit mb-6">
+                    <button @click="addToCart($refs.quantityEl.value)" class="btn-primary py-4 text-lg flex ml-auto min-w-0 w-fit mb-6">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
                         </svg>
