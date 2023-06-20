20a3672b96343152b08004cc676cb41ca54ebd40
wyne-ybanez <wyne_01@icloud.com>
2023-06-20 14:13:14 +0100

    update: add new fonts, add docomposer doctrine/dbal

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     <!-- Font imports-->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Crimson+Text:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
@@ -34,13 +34,7 @@
     </x-footer>
 
     <!-- Toast -->
-    <div 
-    x-data="toast" 
-    x-show="visible" 
-    x-transition 
-    x-cloak 
-    @notify.window="show($event.detail.message)" 
-    class="fixed w-[300px] right-[0.1rem] top-[5.8rem] py-2 px-4 pb-4 bg-emerald-500 text-white">
+    <div x-data="toast" x-show="visible" x-transition x-cloak @notify.window="show($event.detail.message)" class="fixed w-[300px] right-[0.1rem] top-[5.8rem] py-2 px-4 pb-4 bg-emerald-500 text-white">
         <div class="font-semibold" x-text="message"></div>
         <button @click="close" class="absolute flex items-center justify-center right-2 top-2 w-[30px] h-[30px] rounded-full hover:bg-black/10 transition-colors">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
