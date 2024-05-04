16ae0f86de381599e083981dcc4bc966d0d54cea
wyne-ybanez <wyne_01@icloud.com>
2024-05-04 23:23:57 +0100

    build: adds doguhnut container height

--- backend/src/components/core/Charts/Doughnut.vue
+++ backend/src/components/core/Charts/Doughnut.vue
@@ -2,14 +2,7 @@
 import { defineComponent, h } from "vue";
 
 import { Doughnut } from "vue-chartjs";
-import {
-    Chart as ChartJS,
-    Title,
-    Tooltip,
-    Legend,
-    ArcElement,
-    CategoryScale,
-} from "chart.js";
+import { Chart as ChartJS, Title, Tooltip, Legend, ArcElement, CategoryScale } from "chart.js";
 
 ChartJS.register(Title, Tooltip, Legend, ArcElement, CategoryScale);
 
@@ -25,11 +18,11 @@ export default defineComponent({
         },
         width: {
             type: Number,
-            default: 400,
+            default: 300,
         },
         height: {
             type: Number,
-            default: 400,
+            default: 300,
         },
         cssClasses: {
             default: "",
--- backend/src/views/Dashboard.vue
+++ backend/src/views/Dashboard.vue
@@ -1,7 +1,7 @@
 <template>
     <h1 class="text-lg font-normal mb-3">Dashboard</h1>
 
-    <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-3">
+    <div class="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-4 gap-3">
       <!-- Acive Customers -->
       <div class="bg-white py-6 px-5 rounded border text-lg flex flex-col items-center justify-center">
         <label class="font-light">Active Customers</label>
@@ -28,8 +28,21 @@
       <!-- End Total Income -->
     </div>
 
-    <div>
-      <DoughnutChart :data="chartData"/>
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 my-3">
+      <div class="bg-white py-6 lg:col-span-2 px-5 rounded border text-lg flex flex-col items-center justify-center">
+        Products
+      </div>
+      <div class="md:grid grid-cols-1">
+        <div class="bg-white py-6 md:row-span-2 lg:h-[20vw] rounded border text-lg flex flex-col items-center justify-center">
+          <DoughnutChart :data="chartData"/>
+        </div>
+        <div class="bg-white py-6 md:row-span-5 px-5 rounded border text-lg flex flex-col items-center justify-center mt-3">
+          Customers
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+      </div>
     </div>
 </template>
 
